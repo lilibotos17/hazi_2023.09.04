aacf2a514b5762130e9c170b5bdd56a8af47c7ff
lilibotos17 <127288092+lilibotos17@users.noreply.github.com>
2023-09-04 20:22:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function felugroAblak() {
+    alert("Boldog új évet!");
+}
+
+function felugroAblak2() {
+    alert("KÉSZ!!");
+}
+
+function felugroAblak3() {
+    alert("Ma már a második nap van!");
+}
